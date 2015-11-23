a1c7d7bbabe258beb4ed3986b0a55f31dada7bfc
sbashwin <sbashwin@gmail.com>
2015-11-23 23:00:36 +0530

    Visual enhancements to run_all_programs.sh

--- run_all_programs.sh
+++ run_all_programs.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+echo "-------------------Hello World----------------------"
+cat helloworld.c
+echo "----------------------Output------------------------"
 gcc helloworld.c -o helloworld
 ./helloworld
+echo "****************************************************"
+echo "--------------------Addition------------------------"
+cat addition.c
+echo "---------------------Output-------------------------"
 gcc addition.c -o addition
 ./addition
+echo "****************************************************"
+echo "--------------------Arrays--------------------------"
+cat arrays.c
+echo "---------------------Output-------------------------"
 gcc arrays.c -o arrays
 ./arrays
 rm helloworld addition arrays
